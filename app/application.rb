091553ee95d0604365c99d3f9aaf55c25c8d5111
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-29 02:44:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     if time <= 12 
       resp.write "Good Morning!"
     else 
-      resp.write "Good Aftermoon!"
+      resp.write "Good Afternoon!"
       
   end
   resp.finish
